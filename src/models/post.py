7029f76953d8bbd413a2149c4c45f2198ee65c49
mattsanpedro7 <mattsanpedro76@gmail.com>
2018-07-05 16:27:21 -0700

    rename _id var and refactor class

--- src/models/post.py
+++ src/models/post.py
@@ -8,12 +8,12 @@ class Post(object):
 
     # which properties the post should have
     # init: method I am going to create...
-    def __init__(self, blog_id, title, content, author, date=datetime.datetime.utcnow(), _id=None):
+    def __init__(self, blog_id, title, content, author, created_date=datetime.datetime.utcnow(), _id=None):
         self.blog_id = blog_id
         self.title = title
         self.content = content
         self.author = author
-        self.created_date = date
+        self.created_date = created_date
         # uuid is the module
         # uuid4 method to generate => 4 is random
         # .hex 32-character hexadecimal string
@@ -47,12 +47,16 @@ class Post(object):
         # don't have to write "collection="
         # Post.from_mongo('123')
         post_data = Database.find_one(collection='posts', query={'_id': id})
-        return cls(blog_id=post_data['blog_id'],
-                   title=post_data['title'],
-                   content=post_data['content'],
-                   author=post_data['author'],
-                   date=post_data['created_date'],
-                   id=post_data['_id'])
+        # # a lot of repetitive tasks...we can simplify
+        # return cls(blog_id=post_data['blog_id'],
+        #            title=post_data['title'],
+        #            content=post_data['content'],
+        #            author=post_data['author'],
+        #            created_date=post_data['created_date'],
+        #            _id=post_data['_id'])
+
+        # get name and say object element equal to that
+        return cls(**post_data)
 
     @staticmethod
     def from_blog(id):
